5c9b49a889eba24887bbda7ec04e0cb83cf40c89
Maximiliano Delgado <mdelgado@baufest.com>
2021-09-20 00:00:30 -0300

    chore: edit build

--- src/example/example.service.ts
+++ src/example/example.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ExampleService {
-  getHello(): string {
+  getHello() {
     return 'Hello from the new package!';
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+export * from './example/example.module';
 export * from './example/example.service';
