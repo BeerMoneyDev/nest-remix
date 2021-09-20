4d8d075fb2d384103e1cf07d33e8dc4cbd311127
Maximiliano Delgado <mdelgado@baufest.com>
2021-09-20 11:31:46 -0300

    chore: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,6 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
