46982fb6e8439e781ea664fae25ddc4a336331a6
Walaff Fernandes <lncitador@icloud.com>
2023-10-16 12:11:53 -0300

    feat(remix.controller.ts): add versioning support to RemixController
     - Add logic to check for versioning type (URI, MEDIA_TYPE, HEADER)
      - Check if request URL starts with versioning prefix
     - Check if request headers include versioning key
     - Check if request headers include versioning header
     - Check if request URL starts with global prefix
     - Skip processing if request is a static asset

--- src/remix.controller.ts
+++ src/remix.controller.ts
@@ -5,11 +5,14 @@ import {
   Req,
   Res,
   VERSION_NEUTRAL,
+  VersioningType,
 } from '@nestjs/common';
-import { ModuleRef } from '@nestjs/core';
+import { ApplicationConfig, ModuleRef } from '@nestjs/core';
 import { createRequestHandler } from '@remix-run/express';
 import { NextFunction, Request, Response } from 'express-serve-static-core';
+
 import { InjectRemixConfig } from './remix-config';
+
 import type { RemixConfig } from './remix-config';
 import type { GetLoadContextFunction } from '@remix-run/express';
 
@@ -29,6 +32,35 @@ export class RemixController {
     @Res() res: Response,
     @Next() next: NextFunction,
   ) {
+    const appConfig = this.moduleRef.get(ApplicationConfig);
+    const versioning = appConfig.getVersioning();
+
+    if (versioning?.type === VersioningType.URI) {
+      const prefix = versioning.prefix;
+
+      if (prefix && req.url.startsWith(`/${prefix}`)) {
+        return next();
+      }
+    } else if (versioning?.type === VersioningType.MEDIA_TYPE) {
+      const key = versioning.key;
+
+      if (key && req.headers['accept']?.includes(`;${key}`)) {
+        return next();
+      }
+    } else if (versioning?.type === VersioningType.HEADER) {
+      const header = versioning.header.toLocaleLowerCase();
+
+      if (header && req.headers[header]) {
+        return next();
+      }
+    }
+
+    const globalPrefix = appConfig.getGlobalPrefix();
+
+    if (globalPrefix && req.url.startsWith(`/${globalPrefix}`)) {
+      return next();
+    }
+
     if (this.isStaticAsset(req)) {
       return next();
     }
